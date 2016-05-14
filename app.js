89063c4994c3704f192b2e4fdca6b58adf6d4d3a
jason goldfarb <jasonkarlgoldfarb@gmail.com>
2016-05-14 14:59:35 -0600

    Minimal functionality

--- app.js
+++ app.js
@@ -1,49 +1,15 @@
-
-function numberMagic(num){
-  // console.log("hello world");
-  if ( evenNumber(num) ) {
-      console.log(evenNumber(num) );
-
-      // return arguments * 2;
-  } else if ( oddNumber(num)  ) {
-      console.log( oddNumber(num) );
-    // return num * 3;
-  } else if ( noNumber(num) ) {
-    console.log(noNumber(num)) ;
-
-  }
+function lifesMeaning() {
+// console.log("Code monkey");
+// var is42 = "Code like a champion!"
+  var not42 = ["It's a mystery...", "Life is but a dream", "I like Cookies and milk"];
+  // return not42[(Math.floor(Math.random() * (not42.length)))];
+if (arguments[0] === 42) {
+  console.log( "Code like a champion");
+} else {
+  console.log("It's a mystery...")
 }
-// numberMagic()
-function evenNumber (num) {
-  if(num % 2 === 0) {
-    return num * 2
-  }
-  // console.log("Hello World")
 }
-function oddNumber (num) {
-  if(num % 3 === 0) {
-    return num * 3
-  }
+ // else {
 
-}
-
-function noNumber (num) {
-  if(num === 'a') {
-    return 'That is not a number!'
-  }
-}
-
-// numberMagic();
-// numberMagic('a');
 //
-//
-// numberMagic(3);
-// // 9
-// // numberMagic(‘a’);
-// // ‘That’s not a number!’
-//
-// numberMagic(8);
-// // 16
-//
-numberMagic(9)
-// 27
+lifesMeaning(4);
